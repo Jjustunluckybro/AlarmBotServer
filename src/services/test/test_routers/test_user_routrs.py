f95f41f2885c0bc6e5dea3bcabb031621a732d83
Nikita Shagalov <shagalovprog@gmail.com>
2023-09-10 20:26:44 +0300

    parametrize user routers tests

--- src/services/test/test_routers/test_user_routrs.py
+++ src/services/test/test_routers/test_user_routrs.py
@@ -1,72 +1,73 @@
+import pytest
 from httpx import AsyncClient
 from starlette import status
 
 from src.services.test.data import UserTestData as Data
 
 
-async def test_create_user(ac: AsyncClient):
-    r = await ac.post(
-        "/users/create_user",
-        json=Data.test_user_1.dict()
+class TestUserRouters:
+
+    @pytest.mark.parametrize(
+        "code, body",
+        [
+            (status.HTTP_201_CREATED, Data.test_user_1.dict()),
+            (status.HTTP_409_CONFLICT, Data.test_user_1.dict()),
+            (status.HTTP_422_UNPROCESSABLE_ENTITY, {"key": "value"})
+        ]
     )
-    assert r.status_code == status.HTTP_201_CREATED
+    async def test_create_user(self, ac: AsyncClient, code, body):
+        r = await ac.post(
+            "/users/create_user",
+            json=body
+        )
+        assert r.status_code == code
 
+    @pytest.mark.parametrize(
+        "code, user_id, res_body",
+        [
 
-async def test_create_user_negative_conflict(ac: AsyncClient):
-    r = await ac.post(
-        "/users/create_user",
-        json=Data.test_user_1.dict()
+            (
+                    status.HTTP_200_OK,
+                    Data.test_user_1.telegram_id,
+                    Data.test_user_1.dict()
+            ),
+            (
+                    status.HTTP_404_NOT_FOUND,
+                    Data.not_exist_id,
+                    {'detail': 'User not found'}
+            )
+        ]
     )
-    assert r.status_code == status.HTTP_409_CONFLICT
+    async def test_get_user(self, ac: AsyncClient, user_id, code, res_body):
+        r = await ac.get(
+            f"/users/get_user/{user_id}"
+        )
+        assert r.status_code == code
+        assert r.json() == res_body
 
-
-async def test_create_user_negative_invalid_data(ac: AsyncClient):
-    r = await ac.post(
-        "/users/create_user",
-        json={"key": "value"}
+    @pytest.mark.parametrize(
+        "code, user_id",
+        [
+            (status.HTTP_200_OK, Data.test_user_1.telegram_id),
+            (status.HTTP_404_NOT_FOUND, Data.not_exist_id)
+        ]
     )
-    assert r.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    async def test_update_username(self, ac: AsyncClient, user_id, code):
+        r = await ac.patch(
+            f"users/update_username/{user_id}?new_name=NewTestName1"
+        )
+        assert r.status_code == code
 
-
-async def test_get_user(ac: AsyncClient):
-    r = await ac.get(
-        f"/users/get_user/{Data.test_user_1.telegram_id}"
+    @pytest.mark.parametrize(
+        "code, user_id, res_body",
+        [
+            (status.HTTP_200_OK, Data.test_user_1.telegram_id, 1),
+            (status.HTTP_404_NOT_FOUND, Data.not_exist_id, {'detail': 'User not found'})
+        ]
     )
-    assert r.status_code == status.HTTP_200_OK
-    assert r.json() == Data.test_user_1.dict()
-
-
-async def test_get_user_negative_not_found(ac: AsyncClient):
-    r = await ac.get(
-        f"/users/get_user/{Data.not_exist_id}"
-    )
-    assert r.status_code == status.HTTP_404_NOT_FOUND
-
-
-async def test_update_username(ac: AsyncClient):
-    r = await ac.patch(
-        f"users/update_username/{Data.test_user_1.telegram_id}?new_name=NewTestName1"
-    )
-    assert r.status_code == status.HTTP_200_OK
-
-
-async def test_update_username_negative_not_found(ac: AsyncClient):
-    r = await ac.patch(
-        f"users/update_username/{Data.not_exist_id}?new_name=NewTestName1"
-    )
-    assert r.status_code == status.HTTP_404_NOT_FOUND
-
-
-async def test_delete_user(ac: AsyncClient):
-    r = await ac.delete(
-        f"users/delete_user/{Data.test_user_1.telegram_id}"
-    )
-    assert r.status_code == status.HTTP_200_OK
-    assert r.text == "1"
-
-
-async def test_delete_user_negative_not_found(ac: AsyncClient):
-    r = await ac.delete(
-        f"users/delete_user/{Data.not_exist_id}"
-    )
-    assert r.status_code == status.HTTP_404_NOT_FOUND
+    async def test_delete_user(self, ac: AsyncClient, user_id, code, res_body):
+        r = await ac.delete(
+            f"users/delete_user/{user_id}"
+        )
+        assert r.status_code == code
+        assert r.json() == res_body
