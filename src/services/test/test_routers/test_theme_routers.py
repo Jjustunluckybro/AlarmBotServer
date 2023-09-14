8c22fcce6edc053733398f5f0eba937ad3b65bbd
Nikita Shagalov <shagalovprog@gmail.com>
2023-09-14 23:18:36 +0300

    Finish update, get_all tests

--- src/services/test/test_routers/test_theme_routers.py
+++ src/services/test/test_routers/test_theme_routers.py
@@ -56,23 +56,42 @@ class TestThemeRouters:
     @pytest.mark.parametrize(
         "code, user_id, res_body",
         [
-            (status.HTTP_200_OK, Data.user_id, list()),
-            (status.HTTP_404_NOT_FOUND, "qqqqqqqq", list())
+            (status.HTTP_200_OK, Data.user_id, None),
+            (status.HTTP_404_NOT_FOUND, "qqqqqqqq", {"detail": "Themes not found"})
         ]
     )
     async def test_get_all_user_themes(self, ac: AsyncClient, code, user_id, res_body):
+        if code == status.HTTP_200_OK:
+            theme_id = pytest.theme_cash.theme_id_1
+            res_body = [Data.test_theme_model_to_write.convert_to_theme_model(theme_id).dict(by_alias=True)]
+
         r = await ac.get(f"themes/get_all_user_themes/{user_id}")
         assert r.status_code == code
+        assert r.json() == res_body
 
     @pytest.mark.parametrize(
-        "code, theme_id, to_update",
+        "code, theme_id, to_update, res_body",
         [
-            (status.HTTP_200_OK, None, Data.to_update),
-            (status.HTTP_404_NOT_FOUND, Data.non_exist_id, Data.to_update),
-            (status.HTTP_400_BAD_REQUEST, "123456", Data.to_update)
+            (
+                    status.HTTP_200_OK,
+                    None,
+                    Data.to_update,
+                    {"update_count": 1}
+            ),
+            (
+                    status.HTTP_404_NOT_FOUND,
+                    Data.non_exist_id,
+                    Data.to_update,
+                    {"detail": "Theme not found"}
+            ),
+            (
+                    status.HTTP_400_BAD_REQUEST,
+                    "1",
+                    Data.to_update,
+                    {"detail": "1 is not a valid, it must be a 12-byte input or a 24-character hex string"})
         ]
     )
-    async def test_update_theme(self, ac: AsyncClient, code, theme_id, to_update):
+    async def test_update_theme(self, ac: AsyncClient, code, theme_id, to_update, res_body):
         if code == status.HTTP_200_OK:
             theme_id = pytest.theme_cash.theme_id_1
 
@@ -81,6 +100,7 @@ class TestThemeRouters:
             json=to_update
         )
         assert r.status_code == code
+        assert r.json() == res_body
 
     @pytest.mark.parametrize(
         "code, theme_id, res_body",
@@ -100,7 +120,7 @@ class TestThemeRouters:
                     {"detail": "1 is not a valid, it must be a 12-byte input or a 24-character hex string"})
         ]
     )
-    async def test_delete_theme(self, ac: AsyncClient, code, theme_id, res_body):
+    async def test_delete_theme(self, ac: AsyncClient, code, theme_id, res_body):  # TODO
         if code == status.HTTP_200_OK:
             theme_id = pytest.theme_cash.theme_id_1
 
@@ -114,7 +134,7 @@ class TestThemeRouters:
             (status.HTTP_404_NOT_FOUND, Data.non_exist_id, {"detail": "Theme not found"}),
         ]
     )
-    async def test_delete_all_user_themes(self, ac: AsyncClient, code, user_id, res_body):
+    async def test_delete_all_user_themes(self, ac: AsyncClient, code, user_id, res_body):  # TODO
         if code == status.HTTP_200_OK:
             theme_id = pytest.theme_cash.theme_id_1
             for i in range(0, 2):
